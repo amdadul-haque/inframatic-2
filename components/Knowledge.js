8ad7a8a7596e84eb255767a12d8867b222742e2c
Michael Rustell <michael.rustell25@gmail.com>
2024-06-01 14:11:46 +0100

    Update Knowledge.js
    knowledge 4

--- components/Knowledge.js
+++ components/Knowledge.js
@@ -9,6 +9,7 @@ const Knowledge = () => {
   const handleClick = (index) => {
     setIndex((prev) => prev + 1 === knowledge.sections.length ? 0 : prev + 1)
   }
+
   return (
     <div className='py-52 pt-60 bg-sec-2 bg-gradient-to-r from-sec-1 to-prim-1 cursor-pointer'
       onClick={handleClick}
@@ -20,7 +21,8 @@ const Knowledge = () => {
             <p className='text-white-0 pt-4 text-center'>{knowledge.description}</p>
           </div>
         </div>
-        <div className='w-full flex flex-col lg:flex-row lg:justify-between gap-20'>
+        {/* Added more space between the top text and the image */}
+        <div className='mt-20 w-full flex flex-col lg:flex-row lg:justify-between gap-20'>
           <div className='w-full lg:1/2 flex justify-center lg:scale-150'>
             <div className='max-w-[90%] mx-auto relative'>
               <Image src={knowledge?.baseImage} alt='knowledge' width={500} height={500} className='' />
